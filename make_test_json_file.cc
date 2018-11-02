197bbf307ec79d1728326ccbc8ae210246f7044c
keene <1014034085@qq.com>
2018-11-02 23:30:59 +0800

    add json code

---
+++ make_test_json_file.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "./CJsonObject/CJsonObject.hpp"
+#include <string>
+using namespace std;
+
+int main()
+{
+    neb::CJsonObject ojson("{\"num\":\"123\"}");
+    return 0;
+}
+
