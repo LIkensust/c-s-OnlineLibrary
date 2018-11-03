b20a35c17d5837569ecc26d15c6864045f80ff94
LIkensust <1014034085@qq.com>
2018-11-03 15:18:15 +0800

    updata shell

--- create_file.sh
+++ create_file.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-if (( $# != 1));then
-    printf " usage :creat_file.sh [num]\n"
+#按照参数num创建num个文件 
+#由于文件过多 将文件hash到不同的目录中存储
+
+#在cpp文件中的hash逻辑
+#int _hash_(const string& filename)
+#{
+#    int ret = 0;
+#    string::const_iterator it = filename.begin();
+#    while(it != filename.end())
+#    {
+#        ret = ret*3 + (*it) - '0';
+#        it++;
+#    }
+#    return ret % HASHSIZE;
+#}
+
+filenum=1000
+HASHSIZE=1000
+if (( $# < 1 || $# > 2));then
+    printf " usage :creat_file.sh filenum [dirnum 'fefault=1000']\n"
     exit
 fi
+
+if (( $# == 2));then
+    let filenum=$2
+    let HASHSIZE=$2
+fi
+
 num=$1
 printf "\n"
 printf "=====================================\n"
@@ -31,8 +55,17 @@ printf ">create dir ./books/\n"
 bookfile=${basepath}"/books"
 cd $bookfile
 printf ">get in $bookfile\n"
-
 i=0
+
+while (( i<filenum ))
+do
+    dirname="dir"${i}
+    mkdir ${dirname}
+    let i++
+done
+
+hash=0
+let i=0
 while (( i<num ))
 do
     size=$RANDOM
@@ -47,15 +80,18 @@ do
             if (( next==0 ));then
                 next=1
             fi
-        fi    
+        fi
+        (( hash = hash*2 + next ))
         name=$name$next
         let j++
     done
 #    touch $name
 #   将text.json中的内容复制过来 并替换id为当前文件名id
-    cp ${basepath}"/text.json" $name
+    (( hash = hash%HASHSIZE ))
+    printf "${hash}\n"
+    cp ${basepath}"/text.json" ./"dir"$hash/$name
     let i++
-    sed -i "s/9787115147318/${name}/g" $name
+    sed -i "s/9787115147318/${name}/g" ./"dir"$hash/$name
     printf ">[${i}/${num}] creat $name \n" 
 
 done
