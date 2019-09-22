7c1c6ccfb0653392fc615ad623ca177b5e7e68fa
ubuntu <ken@localhost.localdomain>
2019-09-22 15:34:17 +0800

    add mq

--- libnet/net_mq.h
+++ libnet/net_mq.h
@@ -5,9 +5,68 @@
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <string>
-
+#include <time.h>
+#include <assert.h>
+#include <memory>
+#include <string.h> 
 namespace {
     const char DEFAULTMQNAME[] = "/default_mq_name";
+    static const size_t MESSACESIZE = 65536;
+};
+
+class MqData{
+public:
+    //[socket of query][time]
+    MqData(const int socket,const char* data, const size_t size)
+        :mSize(0),
+         mTag(0)
+    {
+        //根据query的socket与当前时间戳生成tag
+        mTag = getTag(time(NULL));
+        mSize = size + sizeof(mTag);
+        PLOG(DEBUG, "size %d, all size %d" ,(int)size, (int)mSize);
+        mData = std::make_shared<char>(mSize);
+        memcpy(mData.get(), &mTag, sizeof(mTag));
+        memcpy(mData.get() + sizeof(mTag), data, size);
+        PLOG(DEBUG, "data is %s | %s", mData.get() + sizeof(mTag), data);
+    }
+
+    MqData(){}
+
+    ~MqData()
+    {
+        mData.reset();
+    }
+
+    void Init(const char* data, size_t size) {
+        mSize = size;
+        mTag = *((const size_t*)(data));
+        mData = std::make_shared<char>(size + sizeof(mTag));
+        memcpy(mData.get(), data, size);
+    }
+
+    std::shared_ptr<char> GetData() {
+        return mData;
+    }
+
+    size_t GetTag() {
+        return mTag;
+    }
+
+    size_t GetSize() {
+        return mSize;
+    }
+
+private:
+    size_t getTag(const int socket) {
+        return (socket >> 3) + time(NULL);
+    }
+
+private:
+    size_t mSize;
+    size_t mTag;
+    std::shared_ptr<char> mData;
+    //char *mData;
 };
 
 class NetMq {
@@ -30,6 +89,8 @@ public:
                       0777, NULL);
         if(mMq != -1) {
             mLinked = true;
+        } else {
+            PLOG(DEBUG, "LINK failed");
         }
         return mLinked;
     }
@@ -41,6 +102,8 @@ public:
         mMq = mq_open(mName.c_str(), O_RDWR, 0777, NULL);
         if(mMq != -1) {
             mLinked = true;
+        } else {
+            PLOG(DEBUG, "LINK failed");
         }
         return mLinked;
     }
@@ -52,10 +115,29 @@ public:
         }
     }
 
+    int Send(MqData& data) {
+        if(mLinked == false) {
+            return -1;
+        }
+        return mq_send(mMq, data.GetData().get(), data.GetSize(), 1);
+    }
+
+    MqData Recv() {
+        assert(mLinked == true);
+        char buff[MESSACESIZE] = {0};
+        unsigned int msg_prio;
+        int size = mq_receive(mMq, buff, MESSACESIZE, &msg_prio);
+        PLOG(DEBUG , "get data size %d", size);
+        mData.Init(buff, size);
+        return mData;
+    }
+
+
 private:
     std::string mName; 
     mqd_t mMq;
     bool mLinked;
+    MqData mData;
 };
 
 #endif
--- libnet/test/testMq.cc
+++ libnet/test/testMq.cc
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
 #include "../net_mq.h"
 using namespace std;
 int main()
 {
-    NetMq q;
-    std::cout << "Hello world" << std::endl;
+    srand((unsigned int)time(NULL));
+    int id = 0;
+    id = fork();
+    if(id > 0) {
+        NetMq q;
+        q.Create();
+        for(int i = 0;i<10;i++) {
+            sleep(rand()%2);
+            char buff[] = "hello world";
+            MqData data(1,buff,sizeof(buff));
+            q.Send(data);
+        }
+        wait(NULL);
+    } else {
+        sleep(1);
+        NetMq q;
+        q.Link();
+        for(int i=0;i<10;i++) {
+            sleep(rand()%2);
+            MqData ret = q.Recv();
+            auto data = ret.GetData();
+            printf("message : [%s]\n", data.get() + sizeof(size_t));
+        }
+    }
     return 0;
 }
 
